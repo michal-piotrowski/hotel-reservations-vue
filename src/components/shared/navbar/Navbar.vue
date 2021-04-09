daefcdf6b11a372a5373eefbad6b7fba64440317
mipiotrowski <mail.piotrowski.m@gmail.com>
2021-04-09 22:41:50 +0200

    landing page working, probably assets for other pages  are present as well

---
+++ src/components/shared/navbar/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-light hr-dark-themed">
+    <div class="navbar-brand">
+      <a class="nav-item" href="#"><img id="nav-logo-img" src="@/assets/HRLogo.png" alt="HRLOGO"/></a>
+    </div>
+    <div class="navbar navbar-toggler">
+      <a class="nav-item" href="#"><img src="@/assets/Sign_in.png" class="nav-img" alt="SIGN IN"/></a>
+      <a class="nav-item" href="#"><img src="@/assets/information.png"  class="nav-img" alt="INFO"/></a>
+      <a class="nav-item" href="#"><img src="@/assets/Map.png" class="nav-img"  alt="MAP"/></a>
+    </div>
+  </nav>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+<style lang="scss" scoped>
+@import 'bootstrap/scss/bootstrap.scss';
+
+@import './Navbar.scss';
+
+</style>  
